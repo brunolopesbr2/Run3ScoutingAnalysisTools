c6184700b6b4aa0277ff2bf368b1708a0db49548
Bruno Lopes <bruno.lopes@cern.ch>
2025-03-11 22:54:26 +0100

    made repacker work

--- HLTScoutingRepackProducer/plugins/HLTScoutingRepackProducer.cc
+++ HLTScoutingRepackProducer/plugins/HLTScoutingRepackProducer.cc
@@ -59,8 +59,8 @@ public:
   using Run3ScoutingTrackCollection = std::vector<Run3ScoutingTrack>;
   using Run3ScoutingVertexCollection = std::vector<Run3ScoutingVertex>;
   
-  template <typename T> using RefCollection = std::vector<edm::Ref<std::vector<T>>>;
-  template <typename T> using RefMap = edm::ValueMap<edm::Ref<std::vector<T>>>;
+  //template <typename T> using RefCollection = std::vector<edm::Ref<std::vector<T>>>;
+  //template <typename T> using RefMap = edm::ValueMap<edm::Ref<std::vector<T>>>;
   
   explicit HLTScoutingRepackProducer(const edm::ParameterSet& params);
   ~HLTScoutingRepackProducer() override = default;
@@ -74,55 +74,56 @@ private:
   Run3ScoutingVertex createScoutingVertex(reco::Vertex const& vertex);  
     
   // register products for reco object and corresponding Value to Ref to original scouting objects
-  template<typename RecoObjectType, typename ScoutingObjectType>
-  void produceWithRef(std::string const& name);
+  //template<typename RecoObjectType, typename ScoutingObjectType>
+  //void produceWithRef(std::string const& name);
   
   // put reco object together with ValueMap to Ref to original scouting object
-  template<typename RecoObjectType, typename ScoutingObjectType>
-  void putWithRef(edm::Event& iEvent, std::string const& name, std::unique_ptr<RefCollection<reco::Vertex>>& recoObjectRef_collection_ptr, std::unique_ptr<std::vector<Run3ScoutingVertex>>& scoutingObject_collection_ptr);
+  //template<typename RecoObjectType, typename ScoutingObjectType>
+  //void putWithRef(edm::Event& iEvent, std::string const& name, std::unique_ptr<RefCollection<reco::Vertex>>& recoObjectRef_collection_ptr, std::unique_ptr<std::vector<Run3ScoutingVertex>>& scoutingObject_collection_ptr);
   
   //edm::EDGetTokenT<Run3ScoutingTrackCollection> scoutingTrack_collection_token_;
   //edm::EDGetTokenT<Run3ScoutingVertexCollection> scoutingPrimaryVertex_collection_token_;
   edm::EDGetTokenT<std::vector<reco::Vertex>> displacedVertices_token_;
+  edm::EDPutTokenT<std::vector<Run3ScoutingVertex>> putToken_;
 
   //bool produce_PFCHSCandidate_; // CHS = charged hadron subtraction
   //bool produce_PFSKCandidate_; // SK = soft killer
   //HepPDT::ParticleDataTable const *particle_data_table_;
 
-  inline static const std::string REF_TO_RECO_LABEL_SUFFIX_ = "-RefToOriginal"; 
+  //inline static const std::string REF_TO_RECO_LABEL_SUFFIX_ = "-RefToOriginal"; 
 };
 
 HLTScoutingRepackProducer::HLTScoutingRepackProducer(edm::ParameterSet const& params)
-  :  displacedVertices_token_(consumes(params.getParameter<edm::InputTag>("displacedVertices")))
+  :  displacedVertices_token_(consumes(params.getParameter<edm::InputTag>("displacedVertices"))),
+     putToken_{produces("scoutingDispalcedVertices")}
 {
 
-  produceWithRef<Run3ScoutingVertex, reco::Vertex>("DisplacedVertex");
+  //produces<std::vector<Run3ScoutingVertex>>("DisplacedVertex");
   
 }
 
-//invert this
+/*
 template <typename ScoutingObjectType, typename RecoObjectType>
 void HLTScoutingRepackProducer::produceWithRef(std::string const& name) {
     produces<std::vector<ScoutingObjectType>>(name);
     produces<RefMap<RecoObjectType>>(name + REF_TO_RECO_LABEL_SUFFIX_);
-}
+    }*/
 
 void HLTScoutingRepackProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   // produce Scouting Vertex
   edm::Handle<std::vector<reco::Vertex>> displacedVertices_handle = iEvent.getHandle(displacedVertices_token_);
-  auto scoutingDisplacedVertex_ptr = std::unique_ptr<std::vector<Run3ScoutingVertex>>();
-  auto recoDisplacedVertexRef_ptr = std::unique_ptr<RefCollection<reco::Vertex>>();
+  std::unique_ptr<std::vector<Run3ScoutingVertex>> scoutingDisplacedVertex_ptr(new std::vector<Run3ScoutingVertex>);
+  //auto recoDisplacedVertexRef_ptr = std::unique_ptr<RefCollection<reco::Vertex>>();
   if (displacedVertices_handle.isValid()) {
-    //std::cout<<"number of scouting vertices: "<<scoutingPrimaryVertex_collection_handle->size()<<std::endl;
     for (size_t DisplacedVertex_index = 0; DisplacedVertex_index < displacedVertices_handle->size(); DisplacedVertex_index++) {
       auto &DisplacedVertex = displacedVertices_handle->at(DisplacedVertex_index);
       scoutingDisplacedVertex_ptr->push_back(createScoutingVertex(DisplacedVertex));
-      recoDisplacedVertexRef_ptr->push_back(edm::Ref<reco::VertexCollection>(displacedVertices_handle, DisplacedVertex_index));
+      //recoDisplacedVertexRef_ptr->push_back(edm::Ref<reco::VertexCollection>(displacedVertices_handle, DisplacedVertex_index));
     }
   }
   
   // put products in Event
-  putWithRef<Run3ScoutingVertex, reco::Vertex>(iEvent, "DisplacedVertex", recoDisplacedVertexRef_ptr, scoutingDisplacedVertex_ptr);
+  iEvent.emplace(putToken_, std::move(*scoutingDisplacedVertex_ptr));
 }
 
 Run3ScoutingVertex HLTScoutingRepackProducer::createScoutingVertex(reco::Vertex const& vertex) {
@@ -167,7 +168,7 @@ Run3ScoutingVertex HLTScoutingRepackProducer::createScoutingVertex(reco::Vertex 
   return scoutingVertex;
 }
 
-
+/*
 template <typename ScoutingObjectType, typename RecoObjectType>
 void HLTScoutingRepackProducer::putWithRef(edm::Event& iEvent, std::string const& name,
                                            std::unique_ptr<RefCollection<reco::Vertex>>& recoObjectRef_collection_ptr, std::unique_ptr<std::vector<Run3ScoutingVertex>>& scoutingObject_collection_ptr) {
@@ -178,13 +179,13 @@ void HLTScoutingRepackProducer::putWithRef(edm::Event& iEvent, std::string const
     filler_refmap_to_reco.insert(scoutingObject_collection_handle, recoObjectRef_collection_ptr->begin(), recoObjectRef_collection_ptr->end());
     filler_refmap_to_reco.fill();
     iEvent.put(std::move(refmap_to_reco), name + REF_TO_RECO_LABEL_SUFFIX_);
-}
+    }*/
 
 
 void HLTScoutingRepackProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
     edm::ParameterSetDescription desc;
     desc.add<edm::InputTag>("displacedVertices", edm::InputTag("Vertexer"));
-    desc.add<edm::InputTag>("scoutingDisplacedVertex", edm::InputTag("DisplacedVertexFromVertexer"));
+    //desc.add<edm::InputTag>("scoutingDisplacedVertex", edm::InputTag("DisplacedVertexFromVertexer"));
     descriptions.addWithDefaultLabel(desc);
 }
 
--- HLTScoutingRepackProducer/plugins/classes.h
+++
@@ -1,3 +0,0 @@
-#include "DataFormats/Common/interface/ValueMap.h"
-#include "DataFormats/Common/interface/Ref.h"
-#include "DataFormats/VertexReco/interface/Vertex.h"
